79452796bbeec2bc0e1da74135ef4a6519b14418
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-10-17 01:06:05 +0900

    :tada: begin a project

---
+++ ModEntry.cs
@@ -0,0 +1,84 @@
+﻿using HarmonyLib;
+using JumpKing.MiscSystems.LocationText;
+using JumpKing;
+using JumpKing.Mods;
+using System.Diagnostics;
+using System;
+using System.Reflection;
+using Steamworks;
+
+namespace JumpKing_CustomMap_i18n
+{
+    [JumpKingMod("YutaGoto.JumpKing_CustomMap_i18n")]
+    public static class ModEntry
+    {
+        private static readonly string harmonyId = "YutaGoto.JumpKing_CustomMap_i18n";
+        public static readonly Harmony harmony = new Harmony(harmonyId);
+
+        /// <summary>
+        /// Called by Jump King before the level loads
+        /// </summary>
+        [BeforeLevelLoad]
+        public static void BeforeLevelLoad()
+        {
+            // Debugger.Launch();
+        }
+
+        /// <summary>
+        /// Called by Jump King when the level unloads
+        /// </summary>
+        [OnLevelUnload]
+        public static void OnLevelUnload()
+        {
+            // Your code here
+        }
+
+        /// <summary>
+        /// Called by Jump King when the Level Starts
+        /// </summary>
+        [OnLevelStart]
+        public static void OnLevelStart()
+        {
+            NewSetSettingsData();
+        }
+
+        /// <summary>
+        /// Called by Jump King when the Level Ends
+        /// </summary>
+        [OnLevelEnd]
+        public static void OnLevelEnd()
+        {
+            // Your code here
+        }
+
+        public static bool NewSetSettingsData()
+        {
+            string text = "gui\\location_settings";
+            string langString = SteamApps.GetCurrentGameLanguage();
+
+            // see: https://partner.steamgames.com/doc/store/localization/languages
+            // english,
+            // SimplifiedChinese,
+            // BrazilianPortugese,
+            // French,
+            // Russian,
+            // German,
+            // Spanish,
+            // TraditionalChinese,
+            // koreana,
+            // japanese
+
+            try
+            {
+                string fileString = Game1.instance.contentManager.root + text + "." + langString + ".xml";
+                AccessTools.StaticFieldRefAccess<LocationSettings>("JumpKing.MiscSystems.LocationText.LocationTextManager:_settings") = XmlSerializerHelper.Deserialize<LocationSettings>(fileString);
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# JumpKing CustomMap I18n
+
+This is a mod to set up the localization of custom maps for JumpKing.
+
+### TODO
+
+- [ ] Level name
+  - [ ] Title
+  - [ ] Menu
+  - [ ] Results
+- [ ] Credits
+- [x] Location settings
+- [ ] OldManSettings > looping_lines > OldManQuote
+- [ ] RattmanSettings > text > lines
